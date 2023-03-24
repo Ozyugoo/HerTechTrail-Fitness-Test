91264b64225309443b4de0275a7f2c37da4b1733
Ozyugoo <stellaozy22@gmail.com>
2023-03-24 16:59:40 +0100

    My first Her Tech Trail project / fitness test

---
+++ scripts.js
@@ -0,0 +1,55 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+});
+
+const change1 = document.getElementById("image2");
+
+change1.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("image3");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "violet";
+    event.target.style.color = "teal";
+});
+
+const change3 = document.getElementById("image4");
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background ="purple";
+    event.target.style.color ="beige";
+});
+
+const change4 = document.getElementById("image5");
+
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "gray";
+    event.target.style.color = "brown";
+});
+
+const change5 = document.getElementById("image6");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "maroon";
+    event.target.style.color = "gold";
+});
+
+const change6 = document.getElementById("image7");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "orange"
+});
+
+const change7 = document.getElementById("image8");
+
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "black";
+});
